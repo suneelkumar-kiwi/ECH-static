2ef4e3aae762420f8745f408fe7bcc4dde40234d
Suneel Kumar <suneel.kumar@kiwitech.com>
2024-11-19 17:52:24 +0530

    Roadmap spanish translation

--- src/components/roadmap.tsx
+++ src/components/roadmap.tsx
@@ -11,16 +11,13 @@ interface RoadmapProps {
 
 function Roadmap({ closeRoadmapModal }: RoadmapProps) {
     const { t } = useTranslation();
-    const router = useNavigate();
-    const roadmapData = t("roadmap.years", { returnObjects: true });
-    const milestones = t("roadmap.milestones", { returnObjects: true });
 
     return (
         <div className="roadmap-modal">
             <div className="header">
                 <Navbar>
                     <Container fluid>
-                        <button className="back-to-home" type="button" onClick={closeRoadmapModal}><Image src={backBtn} alt="Back button" /> Back </button>
+                        <button className="back-to-home" type="button" onClick={closeRoadmapModal}><Image src={backBtn} alt="Back button" /> {t('BACK_TEXT')} </button>
                         <LanguageSelector />
                     </Container>
                 </Navbar>
@@ -34,29 +31,28 @@ function Roadmap({ closeRoadmapModal }: RoadmapProps) {
                     alt="Evolved logo"
                     className="logo-center"
                 />
-                <h1> <span> 5-Year </span> Roadmap </h1>
+                <h1> <span> {t('5_YEAR')} </span> {t('ROADMAP')} </h1>
                 <div className="timeline-wrap">
                     <Row className='row-reverse'>
                         <Col md={6}>
                             <div className="year-title">
-                                <h2> 2021 </h2>
-                                <p> Laying the Foundation </p>
+                                <h2> {t('RM_YEAR_2021')} </h2>
+                                <p> {t('RM_2021_DES')}</p>
                             </div>
                         </Col>
                         <Col md={6}>
                             <div className="year-target">
                                 <div className="quater-target-box">
-                                    <span> Q3 </span>
-                                    <ul>
-                                        <li> Conceptualization of the Evolved Human Care model.</li>
-                                    </ul>
+                                    <span> {t('Q3')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2021_Q3') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q4 </span>
-                                    <ul>
-                                        <li> Development of the physical model concept. </li>
-                                        <li> Comprehensive market research initiated. </li>
-                                    </ul>
+                                    <span> {t('Q4')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2021_Q4') }}
+                                    />
                                 </div>
                             </div>
                         </Col>
@@ -66,36 +62,35 @@ function Roadmap({ closeRoadmapModal }: RoadmapProps) {
                     <Row>
                         <Col md={6}>
                             <div className="year-title">
-                                <h2> 2022 </h2>
-                                <p> Building the Virtual Framework </p>
+                                <h2> {t('RM_YEAR_2022')}  </h2>
+                                <p> {t('RM_2022_DES')} </p>
                             </div>
                         </Col>
                         <Col md={6}>
                             <div className="year-target">
                                 <div className="quater-target-box">
-                                    <span> Q1 </span>
-                                    <ul>
-                                        <li> Virtual model ideation begins.</li>
-                                    </ul>
+                                    <span> {t('Q1')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2022_Q1') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q2 </span>
-                                    <ul>
-                                        <li> Continued virtual model development. </li>
-                                        <li> Founders team formation completed. </li>
-                                    </ul>
+                                    <span> {t('Q2')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2022_Q2') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q3 </span>
-                                    <ul>
-                                        <li> MVP (Minimum Viable Product) modeling initiated. </li>
-                                    </ul>
+                                    <span> {t('Q3')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2022_Q3') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q4 </span>
-                                    <ul>
-                                        <li> Market research and partner scoping. </li>
-                                    </ul>
+                                    <span> {t('Q4')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2022_Q4') }}
+                                    />
                                 </div>
                             </div>
                         </Col>
@@ -105,38 +100,35 @@ function Roadmap({ closeRoadmapModal }: RoadmapProps) {
                     <Row className='row-reverse'>
                         <Col md={6}>
                             <div className="year-title">
-                                <h2> 2023 </h2>
-                                <p> Structuring the Business </p>
+                                <h2> {t('RM_YEAR_2023')}  </h2>
+                                <p> {t('RM_2023_DES')} </p>
                             </div>
                         </Col>
                         <Col md={6}>
                             <div className="year-target">
                                 <div className="quater-target-box">
-                                    <span> Q1 </span>
-                                    <ul>
-                                        <li> Official incorporation in Canada.</li>
-                                    </ul>
+                                    <span> {t('Q1')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2023_Q1') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q2 </span>
-                                    <ul>
-                                        <li> First company valuation completed. </li>
-                                        <li> Investor Round 1 initiated. </li>
-                                    </ul>
+                                    <span> {t('Q2')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2023_Q2') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q3 </span>
-                                    <ul>
-                                        <li> Dubai market entry initiated. </li>
-                                        <li> Investor Round 2, engaging with key partners. </li>
-                                    </ul>
+                                    <span> {t('Q3')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2023_Q3') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q4 </span>
-                                    <ul>
-                                        <li> Participated in several pitching events with Venture Capitalists (VCs). </li>
-                                        <li> Market research and partner scoping.  </li>
-                                    </ul>
+                                    <span> {t('Q4')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2023_Q4') }}
+                                    />
                                 </div>
                             </div>
                         </Col>
@@ -146,43 +138,35 @@ function Roadmap({ closeRoadmapModal }: RoadmapProps) {
                     <Row>
                         <Col md={6}>
                             <div className="year-title">
-                                <h2> 2024 </h2>
-                                <p> Expanding Operations </p>
+                                <h2> {t('RM_YEAR_2024')}  </h2>
+                                <p> {t('RM_2024_DES')} </p>
                             </div>
                         </Col>
                         <Col md={6}>
                             <div className="year-target">
                                 <div className="quater-target-box">
-                                    <span> Q1 </span>
-                                    <ul>
-                                        <li> Core team expansion.</li>
-                                        <li> UAE corporation creation.</li>
-                                    </ul>
+                                    <span> {t('Q1')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2024_Q1') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q2 </span>
-                                    <ul>
-                                        <li> Investor Round 3. </li>
-                                        <li> Finalization of partnerships. </li>
-                                    </ul>
+                                    <span> {t('Q2')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2024_Q2') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q3 </span>
-                                    <ul>
-                                        <li> Integration of Fiat to Cryptocurrency On and Off Ramp through partnership. </li>
-                                        <li> Integration of Blockchain-based Hot Storage Solution through partner.</li>
-                                        <li> Integration of KYC Verification company into the app. </li>
-                                    </ul>
+                                    <span> {t('Q3')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2024_Q3') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q4 </span>
-                                    <ul>
-                                        <li> MVP Beta Launch and Testing. </li>
-                                        <li> 500 practitioners by December. </li>
-                                        <li> 2,000 users by December. </li>
-                                        <li> Token Launch Strategy Discussions. </li>
-                                        <li> Launch of the Deluxe App Service Suite Subscription (PHR & legacy health record features). </li>
-                                    </ul>
+                                    <span> {t('Q4')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2024_Q4') }}
+                                    />
                                 </div>
                             </div>
                         </Col>
@@ -192,41 +176,35 @@ function Roadmap({ closeRoadmapModal }: RoadmapProps) {
                     <Row className='row-reverse'>
                         <Col md={6}>
                             <div className="year-title">
-                                <h2> 2025 </h2>
-                                <p> Scaling and Milestones </p>
+                                <h2> {t('RM_YEAR_2025')}  </h2>
+                                <p> {t('RM_2025_DES')} </p>
                             </div>
                         </Col>
                         <Col md={6}>
                             <div className="year-target">
                                 <div className="quater-target-box">
-                                    <span> Q1 </span>
-                                    <ul>
-                                        <li> Finalization of partnerships for physical clinic locations in Mexico and Canada.</li>
-                                        <li> 1,500 practitioners by January. </li>
-                                        <li> 10,000 paid users by February. </li>
-                                        <li> Token Launch Strategy Discussions. </li>
-                                        <li> Estimated 60,000+ overall users for mid year. </li>
-                                    </ul>
+                                    <span> {t('Q1')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2025_Q1') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q2 </span>
-                                    <ul>
-                                        <li> Official incorporation in Mexico. </li>
-                                        <li> Entry into the LATAM market.  </li>
-                                        <li> Token Launch Strategy Discussions. </li>
-                                    </ul>
+                                    <span> {t('Q2')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2025_Q2') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q3 </span>
-                                    <ul>
-                                        <li> Full integration of the Al Assistant. </li>
-                                    </ul>
+                                    <span> {t('Q3')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2025_Q3') }}
+                                    />
                                 </div>
                                 <div className="quater-target-box">
-                                    <span> Q4 </span>
-                                    <ul>
-                                        <li> Launch of physical clinics in Mexico and Canada. </li>
-                                    </ul>
+                                    <span> {t('Q4')} </span>
+                                    <ul
+                                        dangerouslySetInnerHTML={{ __html: t('RM_2025_Q4') }}
+                                    />
                                 </div>
                             </div>
                         </Col>
@@ -236,23 +214,20 @@ function Roadmap({ closeRoadmapModal }: RoadmapProps) {
                     <Row>
                         <Col>
                             <div className="goals">
-                                <h3> 2025 Milestones Goals </h3>
-                                <ul>
-                                    <li> 5,000 practitioners by December. </li>
-                                    <li> 500,000 paid users by December. </li>
-                                    <li> Estimated 1+ million overall users. </li>
-                                </ul>
+                                <h3> {t('MILESTONE')} </h3>
+                                <ul
+                                    dangerouslySetInnerHTML={{ __html: t('MILESTONE_LIST') }}
+                                />
                             </div>
                         </Col>
                     </Row>
                     <Row>
                         <Col className="flex-end">
                             <div className="goals">
-                                <h3> Key Offerings </h3>
-                                <ul>
-                                    <li> Deluxe App Service Suite Subscription.  </li>
-                                    <li> Personal Health Record (PHR), library access, and legacy health record features with premium subscription. </li>
-                                </ul>
+                                <h3> {t('KEY_OFFERING')} </h3>
+                                <ul
+                                    dangerouslySetInnerHTML={{ __html: t('KEY_OFFERING_LIST') }}
+                                />
                             </div>
                         </Col>
                     </Row>
